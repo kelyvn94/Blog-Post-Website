7d6c6284053dbdb54b58757879a2e24b54c52033
kelvyn94 <kmacharia584@gmail.com>
2024-08-28 23:33:17 +0300

    added some functionalities

--- demo-project/src/Homepage.js
+++ demo-project/src/Homepage.js
@@ -1,17 +1,20 @@
 import { useState } from "react";
 const Homepage = () => {
-    const [name, setName] = useState("Kelvin");
-    const [age, setAge] = useState(24);
+    const [blogs, setBlogs] = useState([
+        {title: 'The power of Node.js', body: 'Node.js empowers developers to build scalable, high-performance web applications with JavaScript, enabling efficient server-side scripting and real-time communication.', author: 'Kelvin', id:1},
+        {title: 'The Oracle Database', body: 'Oracle Database is a multi-model relational database management system, widely used for data storage, processing, and analytics in enterprise applications.', author: 'James', id:2},
+        {title: 'The Transaction Process', body: 'The transaction process involves recording, classifying, and summarizing financial transactions to produce accurate financial statements and ensure proper financial management.', author: 'Peter', id:3}
 
-    const handleClick = (fname) => {
-        setName("James");
-        setAge(30);
-    }
+    ]);
+    
     return ( 
         <div class= "homepage">
-            <h2>HomePage</h2>
-            <p>{name} is {age} old</p>
-            <button onClick={handleClick}>Click Me</button>
+           {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+            <h2>{ blog.title }</h2>
+            <p>Written by { blog.author }</p>
+            </div>
+           ))}
         </div>
      );
 }
